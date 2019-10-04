305096d4bb8e25181348dfa390f3aed0b2cfcd1e
mirei83 <34215921+mirei83@users.noreply.github.com>
2019-10-04 21:16:35 +0200

    MPP-Token

---
+++ token.sol
@@ -0,0 +1,100 @@
+pragma solidity ^0.4.19;
+
+// Simple token contract using the MPP (multi-party payment protocol) feature on the Vechain blockchain.
+// https://github.com/vechain/thor-wiki/blob/master/Prototype(EN).md
+//
+// Enjoy. Make it better. Share.
+//
+// (c) by MiRei - contribute some VET/VTHOR: 0x811a2737cC1D879b2398f1072f262D7499c61964
+//
+
+
+//
+// Vechain MPP modification 1/2:
+// Load builtin contracts for vechain: https://github.com/vechain/thor-builtins
+//
+import "./builtin.sol";
+
+contract ERC20Basic {
+    
+    //
+    // Vechain MPP modification 2/2: 
+    // Use buildin contracts
+    //
+    using Builtin for ERC20Basic;
+
+
+    string public constant name = "MPP Token";
+    string public constant symbol = "MPPT";
+    uint8 public constant decimals = 18;  
+
+
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+    event Transfer(address indexed from, address indexed to, uint tokens);
+
+
+    mapping(address => uint256) balances;
+
+    mapping(address => mapping (address => uint256)) allowed;
+    
+    uint256 totalSupply_;
+
+    using SafeMath for uint256;
+
+
+   constructor(uint256 total) public {  
+	totalSupply_ = total;
+	balances[msg.sender] = totalSupply_;
+    }  
+
+    function totalSupply() public view returns (uint256) {
+	return totalSupply_;
+    }
+    
+    function balanceOf(address tokenOwner) public view returns (uint) {
+        return balances[tokenOwner];
+    }
+
+    function transfer(address receiver, uint numTokens) public returns (bool) {
+        require(numTokens <= balances[msg.sender]);
+        balances[msg.sender] = balances[msg.sender].sub(numTokens);
+        balances[receiver] = balances[receiver].add(numTokens);
+        emit Transfer(msg.sender, receiver, numTokens);
+        return true;
+    }
+
+    function approve(address delegate, uint numTokens) public returns (bool) {
+        allowed[msg.sender][delegate] = numTokens;
+        emit Approval(msg.sender, delegate, numTokens);
+        return true;
+    }
+
+    function allowance(address owner, address delegate) public view returns (uint) {
+        return allowed[owner][delegate];
+    }
+
+    function transferFrom(address owner, address buyer, uint numTokens) public returns (bool) {
+        require(numTokens <= balances[owner]);    
+        require(numTokens <= allowed[owner][msg.sender]);
+    
+        balances[owner] = balances[owner].sub(numTokens);
+        allowed[owner][msg.sender] = allowed[owner][msg.sender].sub(numTokens);
+        balances[buyer] = balances[buyer].add(numTokens);
+        emit Transfer(owner, buyer, numTokens);
+        return true;
+    }
+    
+}
+
+library SafeMath { 
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+      assert(b <= a);
+      return a - b;
+    }
+    
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+      uint256 c = a + b;
+      assert(c >= a);
+      return c;
+    }
+}
